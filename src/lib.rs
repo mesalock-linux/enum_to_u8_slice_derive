a6e07dcbfe26ac8df3f81b262d8526e86058ec39
Yiming Jing <jingyiming@baidu.com>
2018-04-05 14:04:07 -0700

    Change to enum_to_u8_slice_derive

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,25 @@ extern crate quote;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(EnumToStr)]
-pub fn enum_to_str(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(EnumToU8)]
+pub fn enum_to_u8(input: TokenStream) -> TokenStream {
     let s = input.to_string();
     let ast = syn::parse_derive_input(&s).unwrap();
 
     let name = &ast.ident;
     if let syn::Body::Enum(body) = ast.body {
-        let gen = impl_enum_to_str(name, body);
+        let gen = impl_enum_to_u8(name, body);
         gen.parse().unwrap()
     } else {
         panic!("Only work for enum");
     }
 }
 
-fn impl_enum_to_str(name: &syn::Ident, body: Vec<syn::Variant>) -> quote::Tokens {
+fn impl_enum_to_u8(name: &syn::Ident, body: Vec<syn::Variant>) -> quote::Tokens {
     let content = build_content(name, body);
     quote!(
         impl #name{
-            fn enum_to_str(&self) -> &'static [u8] {
+            fn enum_to_u8(&self) -> &'static [u8] {
                 match *self{
                     #content
                 }
